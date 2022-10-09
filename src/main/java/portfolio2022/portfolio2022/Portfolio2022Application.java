dccc270b079d57bbb0e5b4cb94f49f2eeed75aed
noname_dev <noname_dev@naver.com>
2022-10-09 22:24:58 +0900

    포트폴리오

---
+++ src/main/java/portfolio2022/portfolio2022/Portfolio2022Application.java
@@ -0,0 +1,13 @@
+package portfolio2022.portfolio2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Portfolio2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Portfolio2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/portfolio2022/portfolio2022/Portfolio2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio2022.portfolio2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Portfolio2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
