999dd802686a1a1da51402e58e8ba90edb9249b2
noname_dev <noname_dev@naver.com>
2022-10-09 23:38:25 +0900

    DailyShopController

--- src/main/java/portfolio2022/portfolio2022/dailyshop/controller/DailyShopController.java
+++ src/main/java/portfolio2022/portfolio2022/dailyshop/controller/DailyShopController.java
@@ -2,14 +2,19 @@ package portfolio2022.portfolio2022.dailyshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/dailyShop")
 public class DailyShopController {
 
-    @GetMapping("/dailyShop")
+    @GetMapping
     public String home(){
         return "dailyshop/index";
     }
 
-
+    @GetMapping("/product")
+    public String product(){
+        return "dailyshop/product";
+    }
 }
