657b9aa08f7d985b315785879599c5e067de6387
noname_dev <noname_dev@naver.com>
2022-11-30 22:43:04 +0900

    querydsl 플러그인 추가

--- src/main/java/portfolio2022/portfolio2022/portfoliomain/controller/HomeController.java
+++ src/main/java/portfolio2022/portfolio2022/portfoliomain/controller/HomeController.java
@@ -1,10 +1,15 @@
 package portfolio2022.portfolio2022.portfoliomain.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import portfolio2022.portfolio2022.dailyshop.service.MemberService;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final MemberService memberService;
 
     @GetMapping("/")
     public String home(){
