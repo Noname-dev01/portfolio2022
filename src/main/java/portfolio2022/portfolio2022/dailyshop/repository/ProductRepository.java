1a063467f9a2fad0c9228281d5c7351717a01912
noname_dev <noname_dev@naver.com>
2022-12-02 20:49:48 +0900

    상품 리포지토리 추가

---
+++ src/main/java/portfolio2022/portfolio2022/dailyshop/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package portfolio2022.portfolio2022.dailyshop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import portfolio2022.portfolio2022.dailyshop.domain.Product;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
