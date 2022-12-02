cfd56f8167f989febc10fd0f2ba987f80f55788b
noname_dev <noname_dev@naver.com>
2022-12-02 20:51:23 +0900

    상품 서비스 개발

---
+++ src/main/java/portfolio2022/portfolio2022/dailyshop/service/ProductService.java
@@ -0,0 +1,31 @@
+package portfolio2022.portfolio2022.dailyshop.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import portfolio2022.portfolio2022.dailyshop.domain.Product;
+import portfolio2022.portfolio2022.dailyshop.repository.ProductRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Transactional
+    public void saveProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    public List<Product> findProducts(){
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findOne(Long productId){
+        return productRepository.findById(productId);
+    }
+}
