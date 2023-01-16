982a2fdcfa91e85c5211a92045bf01007ecfdf1a
noname_dev <noname_dev@naver.com>
2023-01-16 20:37:52 +0900

    로그인 했을때 로그인 안했을때 보이는 메뉴 다르게 구현

--- src/main/java/portfolio2022/portfolio2022/dailyshop/controller/DailyShopController.java
+++ src/main/java/portfolio2022/portfolio2022/dailyshop/controller/DailyShopController.java
@@ -1,6 +1,8 @@
 package portfolio2022.portfolio2022.dailyshop.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,6 +33,10 @@ public class DailyShopController {
     @GetMapping
     public String home(Model model){
         model.addAttribute("products", productService.findProducts());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.isAuthenticated()){
+            model.addAttribute("username",authentication.getName());
+        }
         return "dailyshop/index";
     }
 
--- src/main/java/portfolio2022/portfolio2022/dailyshop/controller/MemberController.java
+++ src/main/java/portfolio2022/portfolio2022/dailyshop/controller/MemberController.java
@@ -3,7 +3,10 @@ package portfolio2022.portfolio2022.dailyshop.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import portfolio2022.portfolio2022.dailyshop.domain.dto.JoinDto;
 import portfolio2022.portfolio2022.dailyshop.domain.entity.Member;
 import portfolio2022.portfolio2022.dailyshop.service.MemberService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequiredArgsConstructor
@@ -47,39 +53,17 @@ public class MemberController {
 
         return "redirect:/dailyShop/login";
     }
-//    @PostMapping("/join")
-//    public String join(JoinDto joinDto){
-//
-//        Member member = joinDto.toEntity();
-//        member.setRole("ROLE_USER");
-//
-//        Member memberEntity = authService.join(member);
-//        log.info("memberEntity = {}",memberEntity);
-//
-//        return "dailyshop/loginForm";
-//    }
 
-//    @PostMapping("/join")
-//    public String join(@Valid MemberForm form, BindingResult result, DuplicateMemberException e){
-//        if (result.hasErrors()){
-//            return "dailyshop/joinForm";
-//        }
-//
-//        Member memberForm = Member.builder()
-//                .username(form.getUsername())
-//                .password(form.getPassword())
-//                .name(form.getName())
-//                .address(new Address(form.getCity(), form.getStreet(), form.getZipcode()))
-//                .build();
-//
-//        memberService.join(memberForm);
-//        return "redirect:/dailyShop";
-//    }
-//
-//    @GetMapping("/login")
-//    public String loginForm(Model model){
-//        model.addAttribute("member", new MemberForm());
-//        return "dailyshop/loginForm";
-//    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
 
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null){
+            new SecurityContextLogoutHandler().logout(request,response,authentication);
+        }
+
+
+        return "redirect:/dailyShop";
+    }
 }
