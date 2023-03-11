24ac31bc63022fdeea92b54b47858715b6117433
noname_dev <noname_dev@naver.com>
2023-03-11 16:56:05 +0900

    profile Controller 및 테스트 코드

--- src/test/java/portfolio2022/portfolio2022/dailyshop/controller/profile/ProfileControllerTest.java
+++ src/test/java/portfolio2022/portfolio2022/dailyshop/controller/profile/ProfileControllerTest.java
@@ -3,7 +3,12 @@ package portfolio2022.portfolio2022.dailyshop.controller.profile;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.env.MockEnvironment;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -13,6 +18,23 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProfileControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void profile은_인증없이_호출된다() throws Exception{
+        String expected = "default";
+
+        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(expected);
+    }
+
+
     @Test
     public void real_profile이_조회된다(){
         //given
