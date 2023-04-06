dcaf7278e0a1ad73762062a1588ae1723633be63
noname_dev <noname_dev@naver.com>
2023-04-06 17:00:39 +0900

    페이징 처리 테스트 (4)

--- src/main/java/portfolio2022/portfolio2022/dailyshop/controller/ProductController.java
+++ src/main/java/portfolio2022/portfolio2022/dailyshop/controller/ProductController.java
@@ -84,6 +84,7 @@ public class ProductController {
             model.addAttribute("cartListCount", cartItemList.size());
             model.addAttribute("cartItems", cartItemList);
             model.addAttribute("member", member);
+            model.addAttribute("category",category);
             model.addAttribute("productsByCategory", productService.findByCategory(category, pageable, productListCond));
             return "dailyshop/product-list-category";
         }else{
