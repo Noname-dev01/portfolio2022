84465dbbd9181fa2a5b4917c14cd9b6fee12f439
noname_dev <noname_dev@naver.com>
2023-05-02 19:49:42 +0900

    Travis CI -> Actions 로 수정

--- src/main/java/portfolio2022/portfolio2022/dailyshop/service/CategoryService.java
+++ src/main/java/portfolio2022/portfolio2022/dailyshop/service/CategoryService.java
@@ -3,7 +3,6 @@ package portfolio2022.portfolio2022.dailyshop.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import portfolio2022.portfolio2022.dailyshop.domain.entity.Category;
 import portfolio2022.portfolio2022.dailyshop.repository.CategoryRepository;
 
 @Service
@@ -13,9 +12,9 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
-    @Transactional
-    public Long categorySave(Category category){
-        Category saveCategory = categoryRepository.save(category);
-        return saveCategory.getId();
-    }
+//    @Transactional
+//    public Long categorySave(Category category){
+//        Category saveCategory = categoryRepository.save(category);
+//        return saveCategory.getId();
+//    }
 }
